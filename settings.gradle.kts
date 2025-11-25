522c350a7026b144385f4926ab61828023dcd3d3
splahoti12 <slahoti@blackduck.com>
2025-11-26 00:42:06 +0530

    Remove the Plugin management from Settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("org.zaproxy.common.settings") version "0.5.0"
     id("com.diffplug.spotless") version "6.25.0" apply false
